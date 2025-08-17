b6141ed5d6a8142813810a7d1f3506bacb07c84d
kisusu <kikisu115@naver.com>
2025-08-18 01:34:31 +0900

    feat: Initial commit

---
+++ src/main/java/com/helper/library/LibraryBookwormApplication.java
@@ -0,0 +1,13 @@
+package com.helper.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookwormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookwormApplication.class, args);
+	}
+
+}
